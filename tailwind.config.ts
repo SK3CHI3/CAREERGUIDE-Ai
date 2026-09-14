083eebdae6b318dc3192b2c24521381ae4c5f0ba
SK3CHI3 <vomollo101@gmail.com>
2026-09-14 22:58:42 +0300

    fix: lock Georgia font and mobile viewport

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,9 @@ export default {
 			}
 		},
 		extend: {
+			fontFamily: {
+				sans: ['Georgia', 'Times New Roman', 'serif'],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
